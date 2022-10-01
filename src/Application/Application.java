07f008d276458018eed1e9c6976cb432e9da0910
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2022-10-01 15:54:44 +0700

    Update

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -2,12 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package domainmodel;
+package Application;
+
+import Views.ViewSanPham;
 
 /**
  *
  * @author hangnt
  */
-public class SanPham {
-    
+public class Application {
+
+    public static void main(String[] args) {
+        new ViewSanPham().setVisible(true);
+    }
 }
--- src/Services/impl/SanPhamServiceImpl.java
+++ src/Services/impl/SanPhamServiceImpl.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package application;
-
-import view.ViewSanPham;
+package Services.impl;
 
 /**
  *
  * @author hangnt
  */
-public class Application {
-
-    public static void main(String[] args) {
-        new ViewSanPham().setVisible(true);
-    }
+public class SanPhamServiceImpl {
+    
 }
